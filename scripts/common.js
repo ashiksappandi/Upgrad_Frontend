7ecf391ae51b0d38b497b3dde6361ff5c2b1af7d
narayaa <ashik.narayan@ig.com>
2020-06-07 13:49:58 +0530

    Dynamic header.html load

--- scripts/common.js
+++ scripts/common.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
-    $("#header").load("html/header.html");
-    $("#header").load("../html/header.html");
+$(document).ready(function () {
+    if (window.location.pathname.indexOf('index.html') > -1) {
+        $("#header").load("html/header.html");
+         document.addEventListener("DOMContentLoaded", function () {
+             alert('test');
+            document.getElementById('logo-link').removeAttribute('href');
+             document.getElementById('logo-link').setAttribute('href', 'index.html');
+        });
+    }
+    else {
+        $("#header").load("../html/header.html");
+    }
 });
